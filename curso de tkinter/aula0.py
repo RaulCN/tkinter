631305b737da8de7d91f57c7dccc16f537a25f46
Topiarau <31525836+Topiarau@users.noreply.github.com>
2018-11-28 10:16:47 -0200

    Create aula0.py

---
+++ curso de tkinter/aula0.py
@@ -0,0 +1,9 @@
+# Página 5 do pdf https://www.dcc.ufrj.br/~fabiom/mab225/tutorialtkinter.pdf
+from tkinter import * #alterei de Tkinter para tkinter
+class Janela:
+    def __init__(self, instancia_de_Tk):
+        pass
+raiz=Tk()
+Janela(raiz)
+raiz.mainloop()
+
