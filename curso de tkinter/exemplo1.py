63f772509592ca8195a5fae0b613847956383107
Topiarau <31525836+Topiarau@users.noreply.github.com>
2018-11-28 10:25:05 -0200

    Update exemplo1.py

--- curso de tkinter/exemplo1.py
+++ curso de tkinter/exemplo1.py
@@ -6,7 +6,7 @@ class Janela:
     def __init__(self,toplevel):
         self.fr1 = Frame(toplevel)
         self.fr1.pack()
-        self.botao = Button(self.fr1, text='Oi!', background='green')
+        self.botao = Button(self.fr1, text='Oi!', bg='green')
         self.botao.pack()
 raiz=Tk() #N sei pq Tk e não tk
 Janela(raiz)
